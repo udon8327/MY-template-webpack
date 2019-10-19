767039acab367f0146e94767adedab922e9b3103
UDON <udon8327@gmail.com>
2019-10-19 22:27:26 +0800

    加入source-map、CSS解析

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,12 @@
-const path = require("path");
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const devMode = process.env.NODE_ENV !== 'production';
-
 module.exports = {
 	entry: "./src/index.js",
 	output: {
-		path: path.resolve(__dirname, "dist"),
+		path: __dirname + "/dist",
 		filename: "[name].js"
 	},
 	devServer: {
@@ -18,7 +15,10 @@ module.exports = {
 	},
 	module: {
 		rules: [
-			//解析sass
+			{
+				test: /\.css$/,
+				use: ['style-loader','css-loader']
+			},
 			{
 				test: /\.sass$/,
 				use: [
@@ -32,12 +32,10 @@ module.exports = {
 					'sass-loader'
 				],
 			},
-			//解析pug
 			{
 				test: /\.pug$/,
 				use: ['html-loader','pug-html-loader']
 			},
-			//解析圖片
 			{
 				test: /\.(woff|woff2|eot|ttf|otf|png|svg|jpg|gif)$/,
 				use: [
@@ -70,4 +68,5 @@ module.exports = {
 		//自動清空dist資料夾
 		new CleanWebpackPlugin()
 	],
+	// devtool: 'source-map'
 };
