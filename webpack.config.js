335d1b382ede31e17be70ac58dbf019a572553b2
Bohan <bohan.huang@coolbe.com.tw>
2019-10-22 14:35:41 +0800

    整理檔案

--- webpack.config.js
+++ webpack.config.js
@@ -1,97 +1,103 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-	entry: {
-		index: './src/js/index.js',
-		login: './src/js/login.js'
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'js/[name].js',
-	},
-	plugins: [
-		new HtmlWebPackPlugin({
-			template: './src/index.pug',
-			filename: 'index.html',
-			hash: true,
-			chunks: ['vendors','index','bootstrap']
-		}),
-		new HtmlWebPackPlugin({
-			template: './src/login.pug',
-			filename: 'login.html',
-			// hash: true,
-			chunks: ['vendors','login','bootstrap']
-		}),
-		new MiniCssExtractPlugin({
-			filename: 'css/[name].css',
-		}),
-		new webpack.ProvidePlugin({
+  entry: {
+    index: "./src/js/index.js",
+    login: "./src/js/login.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js"
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.pug",
+      filename: "index.html",
+      // hash: true,
+      chunks: ["vendor", "index"]
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/login.pug",
+      filename: "login.html",
+      // hash: true,
+      chunks: ["vendor", "login"]
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css"
+    }),
+    new webpack.ProvidePlugin({
 			$: 'jquery',
-			bootstrap: 'bootstrap'
-		}),
-		new CleanWebpackPlugin(),
-	],
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				// commons: {
-				// 	test: /jquery/,
-				// 	name: 'jquery',
-				// 	chunks: 'all'
-				// },
-				vendors: {
-					test: /[\\/]node_modules[\\/]/,
-					name: "vendors",
-					chunks: 'all'
-				},
-			}
-		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(html|pug|jade)$/,
-				use: ['html-loader','pug-html-loader'],
-			},
-			{
-				test: /\.(s?css|sass)$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {
-							publicPath: '../',
-						},
-					},
-					'css-loader','postcss-loader','sass-loader',
-				],
-			},
-			{
-				test: /\.js$/,
-				use: 'babel-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.(png|jpe?g|gif|svg|webm|mp4|ogg|woff|woff2|ttf|eot)$/,
+			jQuery: 'jquery',
+    }),
+    new CleanWebpackPlugin()
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      cacheGroups: {
+        commons: {
+          chunks: "initial",
+          name: "common",
+          minChunks: 2,
+          maxInitialRequests: 5,
+          minSize: 0
+        },
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendor",
+          chunks: "all"
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(html|pug|jade)$/,
+        use: ["html-loader", "pug-html-loader"]
+      },
+      {
+        test: /\.(s?css|sass)$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../"
+            }
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webm|mp4|ogg|woff|woff2|ttf|eot)$/,
+        use: [
+          {
+            loader: "url-loader",
             options: {
               limit: 8192,
-              name: 'img/[name].[ext]?[hash:7]',
-            },
-          },'image-webpack-loader',
-        ],
-			},
-		]
-	},
-	// devServer: {
-	// 	port: 9000,
-	// 	hot: true,
-	// 	hotOnly: true
-	// },
-	// devtool: 'source-map'
+              name: "img/[name].[ext]?[hash:7]"
+            }
+          },
+          "image-webpack-loader"
+        ]
+      }
+    ]
+  }
+  // devServer: {
+  // 	port: 9000,
+  // 	hot: true,
+  // 	hotOnly: true
+  // },
+  // devtool: 'source-map'
 };
